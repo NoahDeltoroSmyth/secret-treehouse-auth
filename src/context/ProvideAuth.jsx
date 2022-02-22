a3ee108d152756594622d49aadaf1c81febbf2e1
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2022-02-21 16:15:25 -0800

    fixed bug of initial access to protected routes

--- src/context/ProvideAuth.jsx
+++ src/context/ProvideAuth.jsx
@@ -3,7 +3,7 @@ import { createContext, useState } from 'react';
 export const authContext = createContext();
 
 export function ProvideAuth({ children }) {
-  const [user, setUser] = useState([]);
+  const [user, setUser] = useState(null);
 
   const login = (email, password) => {
     const loginSuccessful =
